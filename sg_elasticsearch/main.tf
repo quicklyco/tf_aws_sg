8c8d97a16371549f2e6375ca8a34824b8ac6315d
Selva <nselvakkumaran@users.noreply.github.com>
2015-11-04 13:13:47 -0800

    Update main.tf

--- sg_elasticsearch/main.tf
+++ sg_elasticsearch/main.tf
@@ -1,9 +1,4 @@
 // Provider specific configs
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.region}"
-}
 
 // Security Group Resource for Module
 resource "aws_security_group" "main_security_group" {
